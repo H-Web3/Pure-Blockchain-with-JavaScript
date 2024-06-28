caa01cb030a2efe28425509f0050321a72f549b0
H-Web3 <visualcrn.001@gmail.com>
2024-06-28 03:13:08 -0700

    create three new transactions and a new Block with dummy hashes

--- test.js
+++ test.js
@@ -3,5 +3,12 @@ const Blockchain = require('./blockchain');
 const bitcoin = new Blockchain();// calling the construction function with new keyword and storing it in 'bitcoin' variable 
 
 bitcoin.createNewBlock(1234, 'chscheiohioijsddqwydbccwaq', 'hnxqdwdiyrtnnif3872fwws');//calling the method from constructor function and giving demi parameters 
-console.log(bitcoin);//getting output
+bitcoin.createNewBlock(56789, 'hfidfhiefiefdwerere', 'ifhewiufhfuwehwif');
 
+bitcoin.createNewTransaction(100, 'senderaddressone', 'recepientaddressone');
+bitcoin.createNewTransaction(50, 'senderaddresstwo', 'recepientaddresstwo');
+bitcoin.createNewTransaction(40, 'senderaddressthree', 'recepientaddressthree');
+
+bitcoin.createNewBlock(101112, 'previousblockhash', 'currentblockhash');
+
+console.log(bitcoin);//getting output
