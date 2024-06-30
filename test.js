2a8a5e6b77cb0601ccdabd79ac864ef3b6448139
H-Web3 <visualcrn.001@gmail.com>
2024-06-30 00:51:25 -0700

    Test the createNewBlock method by creating the Genesis Block

--- test.js
+++ test.js
@@ -2,18 +2,4 @@ const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();// calling the construction function with new keyword and storing it in 'bitcoin' variable 
 
-const prevBlockHash = "ndjkjhfuwhcoihfoihsoifuhn";
-const currentBlockData = [
-    {
-        amount: 20,
-        sender: "dskjchweeyhfihwfre",
-        recepient: "oijdnewbvhgorih"
-    },
-    {
-        amount: 10,
-        sender: "kjgschgiyewfheiuofwhefb",
-        recepient: "dshccjbvkjbhvweh"
-    }
-];
-
-console.log(bitcoin.proofOfWork(prevBlockHash, currentBlockData));
+console.log(bitcoin);
