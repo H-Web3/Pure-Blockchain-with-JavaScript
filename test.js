7470b617dc6becd76671e93699f7bc9ccf3a1afd
H-Web3 <visualcrn.001@gmail.com>
2024-06-30 23:24:07 -0700

    nothing but update

--- test.js
+++ test.js
@@ -2,4 +2,17 @@ const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();// calling the construction function with new keyword and storing it in 'bitcoin' variable 
 
-console.log(bitcoin);
+prevBlockHash = 'jcndfbhewiufhy328hfhwecnuheundifu4yh8h38rhruewr';
+currentBlockData = [
+    {
+        amount: 100,
+        sender: 'kjdbuwebfedfew',
+        recepient: 'jnjkebfwubfueb'
+    },
+    {
+        amount: 50,
+        sender: 'dkjshweufhufe',
+        recepient: 'fjcweiufhwiohqrqh8'
+    }
+];
+console.log(bitcoin.hashBlock(prevBlockHash, currentBlockData, 27165));
