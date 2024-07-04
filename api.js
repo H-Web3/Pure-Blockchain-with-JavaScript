f97954051603a3396eb9b18a9026a33684818e2f
H-Web3 <visualcrn.001@gmail.com>
2024-07-04 02:24:29 -0700

    save some little changes

--- api.js
+++ api.js
@@ -1,10 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/blockchainDemo', function (req, res) {
-  res.send('Hello World, coding javascript, see guys!, let start we will go')
-})
-
 app.get('/blockchain', function (req, res) {
   res.send('Here we will show our complete blockchian')
 })
