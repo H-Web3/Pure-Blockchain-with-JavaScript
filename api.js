cef576b2628d1eece368e673dd14a99df9760546
H-Web3 <visualcrn.001@gmail.com>
2024-07-04 02:23:51 -0700

    understand get and post methods and install nodemon'

--- api.js
+++ api.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
 
-/*app.get('/', function (req, res) {
-  res.send('Hello World')
-})*/
+app.get('/blockchainDemo', function (req, res) {
+  res.send('Hello World, coding javascript, see guys!, let start we will go')
+})
 
+app.get('/blockchain', function (req, res) {
+  res.send('Here we will show our complete blockchian')
+})
+
+app.post('/transaction', function (req, res) {
+  res.send('Here we will call our create new transaction method from our blockchain data structure')
+})
 
 app.listen(3000, function()
 {
