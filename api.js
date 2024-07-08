f11292a62c705fd672ad2179ba89e6f1a04435dc
H-Web3 <visualcrn.001@gmail.com>
2024-07-08 04:47:10 -0700

    require blockchain file and make an end point to show all the blocks in browser

--- api.js
+++ api.js
@@ -1,9 +1,11 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const Blockchain = require('./blockchain'); //require the blockchian.js file
+const bitcoin = new Blockchain(); //make an instance of Blockchain constr func and calling it with 'new' keyword
 
-app.get('/blockchain', function (req, res) {
-  res.send('Here we will show our complete blockchian')
-})
+app.get('/blockchain', function (req, res) {   //make an end point 'blockchain'
+  res.send(bitcoin);   //blockchain end point will show us all data available in bitcoin instance
+});
 
 app.post('/transaction', function (req, res) {
   res.send('Here we will call our create new transaction method from our blockchain data structure')
